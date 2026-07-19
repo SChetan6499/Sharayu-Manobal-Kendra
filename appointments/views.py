3364bc1e26cf7fad02d89edf0d6c630ce18bb616
Chetan <chetansamant2017@gmail.com>
2026-07-19 22:30:06 +0530

    google form and other change

--- appointments/views.py
+++ appointments/views.py
@@ -5,127 +5,132 @@ from django.core.mail import send_mail, get_connection
 from .forms import AppointmentForm
 
 
+# def appointment(request):
+
+#     if request.method == "POST":
+
+#         form = AppointmentForm(request.POST)
+
+#         if form.is_valid():
+
+#             appointment = form.save()
+
+#             try:
+#                 connection = get_connection(timeout=10)
+#                 connection.open()
+#                 print("SMTP Connected Successfully")
+#                 connection.close()
+#                 print("EMAIL_HOST_USER:", settings.EMAIL_HOST_USER)
+#                 print("PASSWORD EXISTS:", bool(settings.EMAIL_HOST_PASSWORD))
+#                 # ==========================================
+#                 # Email to Patient
+#                 # ==========================================
+
+# #                 send_mail(
+# #                     subject="Appointment Confirmation - Sharayu Manobal Kendra",
+
+# #                     message=f"""
+# # Dear {appointment.name},
+
+# # Thank you for booking your appointment with Sharayu Manobal Kendra.
+
+# # Your appointment has been received successfully.
+
+# # Appointment Details
+# # ----------------------------
+
+# # Date : {appointment.appointment_date}
+
+# # Time : {appointment.appointment_time}
+
+# # Consultation : {appointment.consultation_type}
+
+# # Our counsellor will contact you shortly to confirm your appointment.
+
+# # Thank you for choosing us.
+
+# # Regards,
+
+# # Sharayu Manobal Kendra
+# # """,
+
+# #                     from_email=settings.DEFAULT_FROM_EMAIL,
+
+# #                     recipient_list=[appointment.email],
+
+# #                     fail_silently=False,
+# #                 )
+
+# #                 # ==========================================
+# #                 # Email to Counsellor / Admin
+# #                 # ==========================================
+
+# #                 send_mail(
+# #                     subject="🔔 New Appointment Booking",
+
+# #                     message=f"""
+# # A new appointment has been booked.
+
+# # ----------------------------------------
+
+# # Patient Name : {appointment.name}
+
+# # Age : {appointment.age}
+
+# # Gender : {appointment.gender}
+
+# # Phone : {appointment.phone}
+
+# # Email : {appointment.email}
+
+# # Consultation : {appointment.consultation_type}
+
+# # Appointment Date : {appointment.appointment_date}
+
+# # Appointment Time : {appointment.appointment_time}
+
+# # ----------------------------------------
+
+# # Problem Description
+
+# # {appointment.problem}
+
+# # ----------------------------------------
+
+# # Please log in to the Django Admin panel to review and manage this appointment.
+# # """,
+
+# #                     from_email=settings.DEFAULT_FROM_EMAIL,
+
+# #                     # Change this later to your clinic email if needed
+# #                     recipient_list=[settings.EMAIL_HOST_USER],
+
+# #                     fail_silently=False,
+# #                 )
+
+#             except Exception as e:
+#                 print("SMTP ERROR:", repr(e))    
+#                 print("Email Error:", e)
+
+#             return redirect("appointment_success")
+
+#     else:
+
+#         form = AppointmentForm()
+
+#     return render(
+#         request,
+#         "appointments/appointment_form.html",
+#         {
+#             "form": form
+#         }
+#     )
+
 def appointment(request):
-
-    if request.method == "POST":
-
-        form = AppointmentForm(request.POST)
-
-        if form.is_valid():
-
-            appointment = form.save()
-
-            try:
-                connection = get_connection(timeout=10)
-                connection.open()
-                print("SMTP Connected Successfully")
-                connection.close()
-                print("EMAIL_HOST_USER:", settings.EMAIL_HOST_USER)
-                print("PASSWORD EXISTS:", bool(settings.EMAIL_HOST_PASSWORD))
-                # ==========================================
-                # Email to Patient
-                # ==========================================
-
-#                 send_mail(
-#                     subject="Appointment Confirmation - Sharayu Manobal Kendra",
-
-#                     message=f"""
-# Dear {appointment.name},
-
-# Thank you for booking your appointment with Sharayu Manobal Kendra.
-
-# Your appointment has been received successfully.
-
-# Appointment Details
-# ----------------------------
-
-# Date : {appointment.appointment_date}
-
-# Time : {appointment.appointment_time}
-
-# Consultation : {appointment.consultation_type}
-
-# Our counsellor will contact you shortly to confirm your appointment.
-
-# Thank you for choosing us.
-
-# Regards,
-
-# Sharayu Manobal Kendra
-# """,
-
-#                     from_email=settings.DEFAULT_FROM_EMAIL,
-
-#                     recipient_list=[appointment.email],
-
-#                     fail_silently=False,
-#                 )
-
-#                 # ==========================================
-#                 # Email to Counsellor / Admin
-#                 # ==========================================
-
-#                 send_mail(
-#                     subject="🔔 New Appointment Booking",
-
-#                     message=f"""
-# A new appointment has been booked.
-
-# ----------------------------------------
-
-# Patient Name : {appointment.name}
-
-# Age : {appointment.age}
-
-# Gender : {appointment.gender}
-
-# Phone : {appointment.phone}
-
-# Email : {appointment.email}
-
-# Consultation : {appointment.consultation_type}
-
-# Appointment Date : {appointment.appointment_date}
-
-# Appointment Time : {appointment.appointment_time}
-
-# ----------------------------------------
-
-# Problem Description
-
-# {appointment.problem}
-
-# ----------------------------------------
-
-# Please log in to the Django Admin panel to review and manage this appointment.
-# """,
-
-#                     from_email=settings.DEFAULT_FROM_EMAIL,
-
-#                     # Change this later to your clinic email if needed
-#                     recipient_list=[settings.EMAIL_HOST_USER],
-
-#                     fail_silently=False,
-#                 )
-
-            except Exception as e:
-                print("SMTP ERROR:", repr(e))    
-                print("Email Error:", e)
-
-            return redirect("appointment_success")
-
-    else:
-
-        form = AppointmentForm()
-
     return render(
         request,
-        "appointments/appointment_form.html",
-        {
-            "form": form
-        }
+        "appointments/appointment_form.html"
     )
-
 
 def appointment_success(request):
 
--- contactus/views.py
+++ contactus/views.py
@@ -3,30 +3,33 @@ from django.shortcuts import render, redirect
 from .forms import ContactForm
 
 
+# def contact(request):
+
+#     if request.method == "POST":
+
+#         form = ContactForm(request.POST)
+
+#         if form.is_valid():
+
+#             form.save()
+
+#             return redirect("contact_success")
+
+#     else:
+
+#         form = ContactForm()
+
+#     return render(
+#         request,
+#         "contact/contact.html",
+#         {
+#             "form": form
+#         }
+#     )
+
+
 def contact(request):
-
-    if request.method == "POST":
-
-        form = ContactForm(request.POST)
-
-        if form.is_valid():
-
-            form.save()
-
-            return redirect("contact_success")
-
-    else:
-
-        form = ContactForm()
-
-    return render(
-        request,
-        "contact/contact.html",
-        {
-            "form": form
-        }
-    )
-
+    return render(request, "contactus/contact.html")
 
 def contact_success(request):
 
